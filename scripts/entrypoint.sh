049cb2d754a0ca28b8583c470c943651b179a089
sdeli <bgfkszmsdeli@gmail.com>
2024-10-11 00:26:38 +0200

    fix/entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -9,8 +9,6 @@ INDEX_FILE="/usr/share/nginx/html/index.html"
 # INDEX_FILE="./index.html" # for dev I leave it here
 
 # Use sed to replace the __SERVER_DATA__ placeholder
-sed -i '' "s|'__SERVER_DATA__'|{VITE_ENV:\"${VITE_ENV}\"}|g" "$INDEX_FILE"
+sed -i "s|'__SERVER_DATA__'|{VITE_ENV:\"${VITE_ENV}\"}|g" "$INDEX_FILE"
 
-
-# Start NGINX
-nginx -g "daemon off;"
+exec nginx -g "daemon off;"
